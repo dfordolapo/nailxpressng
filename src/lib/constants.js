4e5b475f8f5ce0838e0d2474e0b09a662deb004b
dforfolapo <dfordolapo@gmail.com>
2026-07-11 00:40:09 +0100

    Add Admin link to storefront navigation for easy testing

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -25,6 +25,7 @@ export const NAV_LINKS = [
   { label: "Handmade", href: "/handmade" },
   { label: "Factory Made", href: "/factory" },
   { label: "Custom Order", href: SOCIAL_LINKS.whatsapp },
+  { label: "Admin", href: "/admin" },
 ];
 
 export const SORT_OPTIONS = [
