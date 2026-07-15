1a36e4f67d63627997db2ec2a55997a97675e958
dforfolapo <dfordolapo@gmail.com>
2026-07-15 01:48:34 +0100

    Make stiletto shape larger

--- src/components/product/ShapeFilterBar.js
+++ src/components/product/ShapeFilterBar.js
@@ -23,7 +23,7 @@ export default function ShapeFilterBar({ selectedShapes = [], onToggleShape }) {
                   alt={`${shape.name} nail shape`} 
                   fill
                   sizes="40px"
-                  style={{ objectFit: 'contain', transform: 'scale(1.4)' }}
+                  style={{ objectFit: 'contain', transform: shape.id === 'stiletto' ? 'scale(1.7)' : 'scale(1.4)' }}
                 />
               </div>
               <span className={styles.name}>{shape.name}</span>
